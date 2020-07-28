7df4d129d4b6125ab102eac2e8f6abd6acc9f349
Suresh Patil <sureshpatil419@hotmail.com>
2020-07-28 12:43:32 -0400

    Some more code

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [loc, setLoc] = useState(null);
+  const [displayStr, setDisplayStr] = useState('null');
+
+  let a = new Date();
+  let x;
+  
+  if(a.getDay() <= 10) x = 1;
+  else if (a.getDay() <= 20) x = 2;
+  else x = 3;
+
+  let findCoordinates = () => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          let newLoc = JSON.stringify(position);
+          
+          setLoc(newLoc);
+          console.log(newLoc);
+          setDisplayStr('(' + position.coords.longitude.toFixed(4) + ', ' + position.coords.latitude.toFixed(4) + ')');
+        },
+        error => Alert.alert(error.message)
+      )
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{count}</Text>
+      <Button title="Counter" onPress={() => setCount(count + x)}/>
+      <View style={styles.spacing}></View>
+      <Button title="Get Location" onPress={() => findCoordinates()}/>
+      <Text>The current location (Long, Lat): {displayStr}</Text>
     </View>
   );
 }
@@ -13,7 +41,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  spacing: {
+    flex: .05,
+  }
 });
